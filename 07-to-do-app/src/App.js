6e115efb7b6ac13b267d00e5ae4c75b83a140bdd
Sefa Cakir <fikret.sefa@gmail.com>
2022-03-22 10:30:41 +0300

    to do app done

--- 07-to-do-app/src/App.js
+++ 07-to-do-app/src/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import CustomItem from './components/CustomItem';
 
 export default function App() {
 
   const [todos, setTodos] = React.useState([]);
+  const [inputNewTodo, setInputNewTodo] = React.useState("");
 
   const fetchTodos = async () => {
     const res = await axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
@@ -29,17 +31,27 @@ export default function App() {
     
   }
 
-  const deleteTodo = (id) => {
-    const newTodos = { todos: [...todos.filter(todo => todo.id !== id)]}
-    //to:do
-    // axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-    // .then(res => setTodos(newTodos))
-    
+  const deleteTodo = (id) => {  
+    const newTodos =  [...todos.filter(todo => todo.id !== id)]
+    setTodos(newTodos)    
   }
 
 
+  const addTodo = () => {  
+    const newTodo =  {
+      userId: 1,
+      id: uuidv4(),
+      title: inputNewTodo,
+      completed: false
+    };
+    const newTodos =  [...todos, newTodo]
+    setTodos(newTodos)   
+    setInputNewTodo(""); 
+  }
+
   const elements =
-    todos.map((todo, idx) => <CustomItem
+    todos.map((todo, idx) => 
+    <CustomItem
       data={todo}
       markComplete={() => markComplete(todo.id)}
       deleteTodo={() => deleteTodo(todo.id)}
@@ -53,8 +65,8 @@ export default function App() {
   return (
     <div className="container">
       <div className="row">
-        <input className="column column-90" type="text" placeholder="Add Todo.." />
-        <a className="column column-10 button" href="#">ADD TODO</a>
+        <input className="column column-90" type="text" placeholder="Add Todo.." value={inputNewTodo} onChange={(e) => setInputNewTodo(e.target.value)} />
+        <a className="column column-10 button" onClick={addTodo}>ADD TODO</a>
       </div>
       {elements}
     </div>
